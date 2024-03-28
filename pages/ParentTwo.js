c886629b75546a3fb9b58c9d96cb106305186680
yeenathan <yeenathan21@gmail.com>
2024-03-28 14:16:37 -0700

    init

---
+++ pages/ParentTwo.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import ChildTwo from "./ChildTwo";
+
+export default function ParentTwo() {
+    const [data, setData] = useState();
+
+    const childToParent = (childData) => {
+        setData(childData);
+    }
+
+    return(
+        <>
+            {data}
+            <ChildTwo childParent={childToParent}/>
+        </>
+    )
+}
